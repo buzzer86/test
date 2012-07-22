877c14395e60d52627059197723bcea2137ff14f
Paul Burrows <paul_burrows1986@hotmail.co.uk>
2012-07-22 13:11:42 +0100

    test console app

---
+++ GitTest/GitTest.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("GIT test app!");
+        }
+    }
+}
