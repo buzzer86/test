2a619cacffd654aca67528576eab87b8c72cf771
Paul Burrows <paul_burrows1986@hotmail.co.uk>
2012-07-22 13:34:08 +0100

    ?

---
+++ GitTest/GitTest.Console/ProgramNamedChanged.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest.Console
+{
+    class ProgramNamedChanged
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("GIT test app! - updated!");
+        }
+    }
+}
